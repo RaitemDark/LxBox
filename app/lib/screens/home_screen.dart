382f6c78c38ead6599f982f2347deec61eed421b
Raitem <lesnivdanila9@gmail.com>
2026-09-25 13:47:56 +0300

    Fix use_build_context_synchronously properly with an early return

--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1007,16 +1007,15 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver, Ti
                         await _controller.reloadVpn();
                       }
                       
-                      if (mounted) {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Подключение...')),
-                        );
-                        // Go back to main tab to see the connection status
-                        setState(() {
-                          _navIndex = 0;
-                          _nodeList.activeTab = 'Главная';
-                        });
-                      }
+                      if (!mounted) return;
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Подключение...')),
+                      );
+                      // Go back to main tab to see the connection status
+                      setState(() {
+                        _navIndex = 0;
+                        _nodeList.activeTab = 'Главная';
+                      });
                     }
                   },
                   onViewPool: _showPool,
